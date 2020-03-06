74ebf95d20bf5458797af6a16cfdc7594f7bc3a9
Emiliejean <jean.emilie34@hotmail.fr>
2020-03-06 20:15:19 +0100

    essai heroku

--- client/src/router/index.js
+++ client/src/router/index.js
@@ -5,18 +5,4 @@ import HelloWorld from '@/components/HelloWorld'
 import Register from '@/components/Register'
 Vue.use(Router)
 
-export default new Router({
-  routes: [
-    {
-      path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
-    },
-{
-    path: '/register',
-    name: 'register',
-    component: Register
-  }
-  ]
-})
 /* eslint-disable */
